7c0b6d4070d7ea9d7f70375640ddb9ba50a485f6
bminhdat <bminhdat03.p@gmail.com>
2024-05-25 13:56:32 +0700

    Initial

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         Game game = new Game();
+        System.out.println();
     }
 
 }
